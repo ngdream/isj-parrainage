c4c1c27f030b70bd3ebabf9f668a9f68c9ae3de9
ngdream <nguewofoss@gmail.com>
2022-09-24 06:55:20 +0100

    some test

--- Operation.js
+++ Operation.js
@@ -1,3 +1,4 @@
+var current=0
 //class  for fiole
 class Fiole
 {
@@ -17,12 +18,16 @@ class Parrain
     {
         this.name = name;
         this.photo = photo;
+        this.fiole=[]
         Parrain.list.push(this);
     }
 }
 
 Parrain.list =[];
-Fiole.list =[];
+Fiole.list = [];
+
+Fiole.nbrx = 0;
+Parrain.nbrx=0
 
 
 var fphoto = document.getElementById("img_fiole")
@@ -31,32 +36,73 @@ var pphoto = document.getElementById("img_parrain")
 var fname = document.getElementById("name_fiole")
 var pname = document.getElementById("name_parrain")
 
-k=new Parrain("hhh", "ggggg")
-new Parrain("yoo", "yrtrt")
+k = new Parrain("hhh", "test.png")
+k=new Parrain("hhh", "test2.png")
 
 
-new Fiole("yoooo", "dfd")
-new Fiole("ddd","qdfsfsff")
+
+
+new Fiole("ddd", "test.png")
+new Fiole("ddd", "test4.png")
+new Fiole("ddd", "test2.png")
+new Fiole("ddd", "test.png")
+new Fiole("ddd", "test4.png")
+new Fiole("ddd", "test2.png")
+new Fiole("ddd", "test.png")
+new Fiole("ddd", "test4.png")
+new Fiole("ddd", "test2.png")
+new Fiole("ddd", "test.png")
+new Fiole("ddd", "test4.png")
+new Fiole("ddd", "test2.png")
+
+
 function choose()
 {
-    var c=window.setInterval(
-        () => {
-           /* var ptfiol = Fiole.list[Math.random() % (Fiole.list.length-1)]
-            fphoto.scr = ptfiol.photo;*/
-           
-            var ptparrain = Parrain.list[Math.floor((Math.random() * (Parrain.list.length)))]
-            console.log()
-            pphoto.src = ptparrain.photo;
-            pname.textContent = ptparrain.name;
 
+    
+    var ptparrain = Parrain.list[current++]
+    pphoto.src = ptparrain.photo
+    
+    var j=0
+    var chooseinterval=window.setInterval(
+        () => {
             var ptfiol = Fiole.list[Math.floor((Math.random() * (Fiole.list.length)))]
             fphoto.src = ptfiol.photo;
             fname.textContent = ptfiol.name;
 
-            },100)
+            j += 1
+            console.log(j)
+
+            if (j>= 200)
+            {
+                choosefiol(ptfiol, ptparrain)
+                window.clearInterval(chooseinterval) 
+            }
+        }, 100) 
+    
+    var choosefiol = (f,p) =>
+    {
+        f.parrain.push(p)
+        p.fiole.push(f)
+        if (f.parrain.length > Fiole.nbrx)
+            Fiole.nbrx = f.parrain.length
+        if (p.fiole.length > Parrain.nbrx)
+            Fiole.nbrx = p.fiole.length
+        
+              
+        
+       }
 }
 
-choose();
+
+
+
+
+document.getElementById("start").addEventListener("click", () => {
+    choose();
+
+})
+
 
 
 
